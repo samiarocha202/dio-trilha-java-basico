04bfa1e1a70e26555bfa076f3bafc6352460cc79
samiarocha202 <samiasamiaabreu@gmail.com>
2024-05-02 14:57:57 -0300

    My project bank account in cmd

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
- *
  * @author Sâmia Abreu Rocha
  */
 public class ContaTerminal {
-    
+
+    public static void main(String[] args) {
+        
+
+    }
+
 }
